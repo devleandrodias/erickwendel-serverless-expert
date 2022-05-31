c7838cca05891052f6da92c52cb1a01d2feafc08
devleandrodias <leandrodbdias@gmail.com>
2022-05-30 21:11:52 -0300

    feat: implementing serverless framework

---
+++ v1/demo02/run.sh
@@ -0,0 +1,25 @@
+# instalar
+
+npm i -g serverless@1.67
+
+# inicializar
+
+sls
+
+# sempre fazer deploy antes de tudo para verificar se está ok o ambiente
+
+# invocar na aws
+
+sls invoke -f hello
+
+# invocar local
+
+sls invoke local -f hello
+
+# configurar dashboard
+
+sls
+
+# remover recursos
+
+sls remove
