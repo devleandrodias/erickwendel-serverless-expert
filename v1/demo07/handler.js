eec5cffecec3e0dd97f39cd7bf734518b99a81f7
devleandrodias <leandrodbdias@gmail.com>
2022-06-17 10:38:44 -0300

    feat: finish project demo07

---
+++ v1/demo07/handler.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const AWS = require("aws-sdk");
+
+const host = process.env.LOCALSTACK_HOST || "localhost";
+const s3Port = process.env.S3_PORT || "4566";
+
+const s3Config = {
+  s3ForcePathStyle: true,
+  endpoint: new AWS.Endpoint(`http://${host}:${s3Port}`),
+};
+
+const s3 = new AWS.S3(s3Config);
+
+module.exports.hello = async (event) => {
+  const allBuckets = await s3.listBuckets().promise();
+
+  console.log("Buckets founded", allBuckets);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ allBuckets }, null, 2),
+  };
+};
